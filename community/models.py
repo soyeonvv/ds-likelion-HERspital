084b44eb3aa9f6d15a2302b633713bf26ddcd647
soosoo030 <whereru511@gmail.com>
2021-08-06 04:39:13 +0900

    community 글 table 생성 및 READ

--- community/models.py
+++ community/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Community(models.Model):
+    # 제목
+    title = models.CharField(max_length=200)
+    # 공개설정
+    publicSetting = models.BooleanField()
+    # 비밀번호
+    password_Post = models.IntegerField(blank=True, null=True)
+    # 연령 태그
+    age_tag = models.CharField(max_length=100)
+    # 사진
+    image = models.ImageField(upload_to = "community/", blank=True, null=True)
+    # 내용
+    body = models.TextField()
+    # 작성 날짜
+    pub_date = models.DateTimeField()
+    # 작성자 <- 익명으로 표시되지만, '자신이 쓴 글 보기'와 '성별 표시'를 위해서 글 작성자의 정보를 저장해야 함
+        # 사용자 DB가 생성되고 난 후, 만들 예정
+    def __str__(self):
+        return self.title
+    
+    def summary(self): #제목 글자수가 길면 끊어서 보여주도록
+        return self.title[:30]
--- community/urls.py
+++ community/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views as community_views
+from community.views import *
 
 app_name = "community"
 
 urlpatterns = [
-    path("detail/", community_views.detail, name="detail"),
+    # path("detail/", community_views.detail, name="detail"),//
     path("communityList/", community_views.communityList, name="communityList"),
     path("expertList/", community_views.expertList, name="expertList"),
     path("consulting/", community_views.consulting, name="consulting"),
     path("communityWrite/", community_views.communityWrite, name="communityWrite"),
+    path('detail/<str:id>', community_views.detail, name="detail"),
 ]
