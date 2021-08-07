3dbdc950770072307832af56e59298342c9bde15
allzero <0fcoursewhynot@duksung.ac.kr>
2021-08-08 04:51:51 +0900

    회원가입-로그인-메인페이지 백엔드 작업

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
+from django.urls.conf import include
 from . import views as account_views
+from account.views import *
+# from django.contrib.auth import views as account_views
 
 app_name = "account"
 
 urlpatterns = [
-    path("signup/", account_views.signup, name="signup"),
-    path("login/", account_views.login, name="login"),
+    path("signup/", account_views.register_view, name="signup"),
+
+    path("login/", account_views.login_view, name="login"),
+
+    path("logout/", account_views.logout_view, name="logout"),
+
     path("mypage/", account_views.mypage, name="mypage"),
     path("setting/", account_views.setting, name="setting"),
     path("stchange/", account_views.stchange, name="stchange"),
--- herspital/urls.py
+++ herspital/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,7 @@ urlpatterns = [
     path("community/", include("community.urls", namespace="community")),
     #core
     path("core/", include("core.urls", namespace="core")),
+    #runserver했을 시 뜨는 메인페이지
+    path("",views.mainpage,name="mainpage"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
