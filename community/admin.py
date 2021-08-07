394cc9b6a8a140a3bbd1880013dbc47e86c46b23
boylee-17 <leeby1717@gmail.com>
2021-08-07 18:30:31 +0900

    expert list n detail

--- community/admin.py
+++ community/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Community
+from .models import Expert
 # Register your models here.
 admin.site.register(Community)
+admin.site.register(Expert)
