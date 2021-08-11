d720b2e1ca6e979619bffc055d089365972ca6b8
soosoo030 <whereru511@gmail.com>
2021-08-11 16:26:31 +0900

    review테이블 생성 및 연결

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views as core_views
+from core.views import *
 
 app_name = "core"
 
 urlpatterns = [
     path("mainpage/", core_views.mainpage, name="mainpage"),
+    path("create_review", core_views.create_review, name="create_review"),
 ]
